234e004ae3331b82b758b9181cbdc53f3f436351
Jon <jonlgolden@gmail.com>
2019-03-09 18:53:58 -0700

    Foundational managers: Purchase System and Purchase UI manager. Basic functionality and debug buttons plugged in.

---
+++ Assets/Scripts/PurchaseSystemManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseSystemManager : MonoBehaviour {
+
+	//for this code test, the list of possible purchases is simple a public array that can be modified from the Unity Editor
+	//In a real application, this would be procedurally modified based on server data etc
+	public PurchasableItem[] m_available_purchases;
+
+	public List<PurchasableItem> getAvailablePurchases()
+	{
+		//rather than looking directly at the m_available_purchases array, this method returns a nice clean list, and does a little error checking
+		//to avoid handing off bad data to the UI system or whatever else asks for it
+		
+		List<PurchasableItem> purchase_list = new List<PurchasableItem>();
+
+		foreach(PurchasableItem purchase in m_available_purchases)
+		{
+			//if the purchase data is nul for some reason, just skip it
+			if(purchase == null)
+			{
+				continue;
+			}
+
+			
+			string unique_purchase_key = purchase.purchase_key;
+
+		}
+
+		return purchase_list;		
+	}
+
+}
---
+++ Assets/Scripts/PurchaseUIManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PurchaseUIManager : MonoBehaviour {
+
+	public Text m_currency_display;
+	public PurchaseSystemManager m_purchase_manager;
+
+	private int m_currency_owned;
+	
+	void Start () 
+	{
+		//ideally this would cal into some sort of local or server side userdata manager, for now Ill just call into Unity playerPrefs directly
+		m_currency_owned = PlayerPrefs.GetInt("owned_currency");
+		updateCurrencyDisplay();
+	}
+	
+
+	#region Display Methods
+
+	private void updateCurrencyDisplay()
+	{
+		m_currency_display.text = m_currency_owned.ToString();
+	}
+
+	#endregion 
+
+
+	#region Button Methods
+
+	public void debugFreeMoney()
+	{
+		m_currency_owned += 1000;
+		writeCurrencyToUserdata();
+		updateCurrencyDisplay();
+	}
+
+	public void debugUpdateScrollview()
+	{
+		List<PurchasableItem> purchase_list = m_purchase_manager.getAvailablePurchases();
+	}
+
+	#endregion
+
+	#region Internal Utility Methods
+
+	private void writeCurrencyToUserdata()
+	{
+		PlayerPrefs.SetInt("owned_currency", m_currency_owned);
+	}
+
+	#endregion
+	
+}
